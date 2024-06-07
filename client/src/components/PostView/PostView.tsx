d30119bcad51e8b7bfb1512200e75de65e49a8bb
Miurionochek <ivanfedoniukwork@gmail.com>
2024-06-07 09:25:40 +0300

    update design, add delete and edit icons

--- client/src/components/PostView/PostView.tsx
+++ client/src/components/PostView/PostView.tsx
@@ -1,22 +1,33 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { Post } from "../../interfaces/postInterfaces";
-import { Link } from "react-router-dom";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
 import styles from "./PostView.module.scss";
 
 export default function PostView({ id, title, body }: Post) {
   return (
-    <Link to={`/post/${id}`}>
-      <Box className={styles.post}>
-        <ul className={styles.postData}>
-          <li className={styles.dataPiece}>
-            <Box>Title: {title}</Box>
-          </li>
-          <li className={styles.dataPiece}>
-            <Box>Description: {body}</Box>
-          </li>
-        </ul>
-      </Box>
-    </Link>
+    <Box className={styles.post}>
+      <ul className={styles.postData}>
+        <li className={styles.dataPiece}>
+          <Box>Title: {title}</Box>
+        </li>
+        <li className={styles.dataPiece}>
+          <Box>Description: {body}</Box>
+        </li>
+        <li>
+          <Box className={styles.manipulate}>
+            <Button>
+              <DeleteIcon />
+            </Button>
+            <Button>
+              <EditIcon />
+            </Button>
+            <Button variant="contained">Details</Button>
+          </Box>
+        </li>
+      </ul>
+    </Box>
   );
 }
--- client/src/components/UserView/UserView.tsx
+++ client/src/components/UserView/UserView.tsx
@@ -1,29 +1,39 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 import { UserViewData } from "../../interfaces/userInterfaces";
 
 import styles from "./UserView.module.scss";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function UserView({ id, name, email, phone }: UserViewData) {
+  const navigate = useNavigate();
+  const showPosts = () => {
+    navigate(`/${id}/posts`);
+  };
+
   return (
-    <Link to={`/${id}/posts`}>
-      <Box className={styles.user}>
-        <Box className={styles.imageBlock}>
-          <img src="/man-user.svg" alt="user-icon" />
-        </Box>
-        <ul className={styles.userData}>
-          <li className={styles.dataPiece}>
-            <Box>Name: {name}</Box>
-          </li>
-          <li className={styles.dataPiece}>
-            <Box>Email: {email}</Box>
-          </li>
-          <li className={styles.dataPiece}>
-            <Box>Phone: {phone}</Box>
-          </li>
-        </ul>
+    <Box className={styles.user}>
+      <Box className={styles.imageBlock}>
+        <img src="/man-user.svg" alt="user-icon" />
       </Box>
-    </Link>
+      <ul className={styles.userData}>
+        <li className={styles.dataPiece}>
+          <Box>Name: {name}</Box>
+        </li>
+        <li className={styles.dataPiece}>
+          <Box>Email: {email}</Box>
+        </li>
+        <li className={styles.dataPiece}>
+          <Box>Phone: {phone}</Box>
+        </li>
+        <li>
+          <Box>
+            <Button variant="contained" onClick={showPosts}>
+              Posts
+            </Button>
+          </Box>
+        </li>
+      </ul>
+    </Box>
   );
 }
